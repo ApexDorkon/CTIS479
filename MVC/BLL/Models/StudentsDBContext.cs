da0883fe20a2278603cf9ad01c0ababad5d22036
Doruk <dorukakabay@gmail.com>
2024-10-07 01:13:53 +0300

    Homework Done

---
+++ MVC/BLL/Models/StudentsDBContext.cs
@@ -0,0 +1,38 @@
+﻿
+#nullable disable
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLL.Models;
+
+public partial class StudentsDBContext : DbContext
+{
+    public StudentsDBContext(DbContextOptions<StudentsDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC075A66FE20");
+
+            entity.Property(e => e.BirthDate).HasColumnType("datetime");
+            entity.Property(e => e.GPA).HasColumnType("decimal(9, 1)");
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Surname)
+                .IsRequired()
+                .HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
